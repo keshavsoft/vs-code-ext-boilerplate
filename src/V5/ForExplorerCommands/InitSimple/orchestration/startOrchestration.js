d717113a736ca11d1192ad2c1acea18dc820cc8e
keshavsoft <nknnkumar@yahoo.com>
2026-05-26 16:23:06 +0530

    success.html

--- src/V5/ForExplorerCommands/InitSimple/orchestration/startOrchestration.js
+++ src/V5/ForExplorerCommands/InitSimple/orchestration/startOrchestration.js
@@ -1,6 +1,7 @@
 import { finalize, fail } from '../utils/response.js';
 import { copyTemplate } from '../services/copyTemplate.js';
 import { runPrechecks } from '../services/precheck.js';
+import { showSuccessPage } from '../services/showSuccessPage.js';
 
 export async function startOrchestration(uri) {
     try {
@@ -9,6 +10,8 @@ export async function startOrchestration(uri) {
         runPrechecks({ targetPath });
 
         copyTemplate({ targetPath });
+
+        showSuccessPage();
 
         finalize({
             message: `Node API initialized successfully 🚀\nFiles created: package.json, app.js\nLocation: ${targetPath}`
---
+++ src/V5/ForExplorerCommands/InitSimple/services/showSuccessPage.js
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+import fs from 'fs';
+import path from 'path';
+
+export function showSuccessPage() {
+    const htmlPath = path.join(
+        import.meta.dirname,
+        "../html/success.html"
+    );
+    const htmlPath1 = path.join(
+        process.cwd(),
+        "src",
+        "V5",
+        "ForExplorerCommands",
+        "InitSimple",
+        "html",
+        "success.html"
+    );
+
+    const htmlContent = fs.readFileSync(htmlPath, "utf8");
+
+    const panel = vscode.window.createWebviewPanel(
+        "ksExpressSuccess",
+        "KS Express Boilerplate",
+        vscode.ViewColumn.One,
+        {}
+    );
+
+    panel.webview.html = htmlContent;
+}
